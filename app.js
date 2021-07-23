eaad508a4c21a2ce7c7307fdcc4dc12a749fbab5
boljibeatz <salabiubolaji@gmail.com>
2021-07-22 20:00:28 -0400

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Mr daniel is not in Class for today !'));
+app.get('/', (req, res) => res.send('Mr favour is the best teacher!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

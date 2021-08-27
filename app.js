1c28f37d91b5b3fb5e27884e4a0b31c9516578f5
boljibeatz <salabiubolaji@gmail.com>
2021-08-27 08:28:26 -0400

    6TH message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('i love kemi all the time!'));
+app.get('/', (req, res) => res.send('CLOUDELITE IS THE BEST PLACE TO BE!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

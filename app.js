e7deef7d0007f4aaea73c2bba8d07e814b88c313
boljibeatz <salabiubolaji@gmail.com>
2021-09-02 17:18:51 -0400

    8th message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('ALL THANKS TO MR DANIEL N MR FAVOUR!'));
+app.get('/', (req, res) => res.send('I LOVE AWS SERVICES!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

bd15568720753d2efac7c4e162b5113d89dd4d83
boljibeatz <salabiubolaji@gmail.com>
2021-09-04 13:42:22 -0400

    new product

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('HELLO WORLD!'));
+app.get('/', (req, res) => res.send('GOOD DAY MY PEOPLE!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

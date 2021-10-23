c758bbf6afdafe7dfc830e3e156c13be4d0e2d96
jnogieru <joenog1@yahoo.com>
2021-10-22 17:16:54 -0700

    version8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('God is the greatest and I belive so.. Boljibeat my man is also a witnesses of His faithfulness!'));
+app.get('/', (req, res) => res.send('God is the greatest and I belive so.. Boljibeat my man is also a witnesses of Gods faithfulness!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
